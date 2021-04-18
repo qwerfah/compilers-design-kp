31b86a1ae256c2c830b50d660796dc01794fd427
qwerfah <qwerfah@gmail.com>
2021-04-18 12:29:03 +0300

    Initial commit.

---
+++ ScalaCompiler/ScalaCompiler/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ScalaCompiler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
